74414aa4a2cf81807ae4d77bffcb75410f509300
Dani Lyn <dlyn1110@gmail.com>
2018-04-11 15:24:46 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-
+  counter = 0
+  if counter < collection.length
+    yield(collection[counter])
+    counter +=1
+    return 
+    true 
+  else 
+    false
 end
